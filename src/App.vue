4c86216c1cb5ef250e247cfa133b9a4ba62f064a
たのけん <tanoken729@gmail.com>
2021-03-15 09:56:27 +0900

    タスク追加機能作成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Todoリスト</h1>
+    <div>
+      <label><input type="radio" name="change-displey" onclick="changeDispley();" checked>すべて</label>
+      <label><input type="radio" name="change-displey" onclick="changeDispley();" >作業中</label>
+      <label><input type="radio" name="change-displey" onclick="changeDispley();" >完了</label>
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>コメント</th>
+          <th>状態</th>
+        </tr>
+        <tr v-for="(list, i) in lists" v-bind:key="i">
+          <td>{{ i }}</td>
+          <td>{{ list }}</td>
+          <td><button>作業中</button></td>
+          <td><button>削除</button></td>
+        </tr>
+      </table>
+    </div>
+    <h1>新規タスクの追加</h1>
+    <div>
+      <form v-on:submit.prevent>
+          <input type="text" id="todo">
+          <input type="submit" value="送信" @click="addTodo">
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  data() {
+    return {
+      lists: []
+    }
+  },
+  methods: {
+    addTodo: function(){
+      this.lists.push(document.getElementById("todo").value);
+    },
+  },
+};
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+</script>
